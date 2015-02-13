4f41ca191259e5742758901e7c8dad67ff0b995b
kupa25@live.com <haran802>
2015-02-13 07:37:58 -0500

    boiler pages

---
+++ WeddingPlanner/WeddingPlanner.WindowsPhone/AddEvent.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace WeddingPlanner
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AddEvent : Page
+    {
+        public AddEvent()
+        {
+            this.InitializeComponent();
+
+            //Get the location in the background
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void btnCreateEvent_Click(object sender, RoutedEventArgs e)
+        {
+            //Retrieve the location of the event
+            //Store the information in Azure mobile service
+
+            //Display and email the join code for guests
+            //Navigate to the wedding page
+        }
+    }
+}
